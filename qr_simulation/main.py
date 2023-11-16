e14abb72ff1f96a70d29430d8a6c7e9757162c98
A01749675 <A01749675@tec.mx>
2023-11-16 14:03:40 -0600

    update on position based on cmdvel

--- qr_simulation/main.py
+++ qr_simulation/main.py
@@ -3,24 +3,31 @@ from rclpy.node import Node
 from std_msgs.msg import Int8,Bool,Float64
 from geometry_msgs.msg import Twist
 from .submodules.alvinxy import *
-
+import math
+import numpy as np
 class Main(Node):
     def __init__(self):
-        self.subscribe = self.create_subscription(Twsit,"cmd_vel",self.update_velocity)
+        self.subscribe = self.create_subscription(Twist,"cmd_vel",self.update_velocity)
         self.linear_velocity = 0.0
         self.angular_velocity = 0.0
         self.angle = 0.0
         self.refresh_rate = 0.01
         self.orglat = 19.5970212
         self.orglong = -99.227144
+        self.x = 0.0
+        self.y = 0.0
+        self.vx = 0.0
+        self.vy = 0.0
         
     
-    def update_velocity(self,msg):
-        self.linear_velocity = msg.linear.x
-        self.angle += msg.angular.z*self.refresh_rate 
+    def update_data(self,data):
+        self.angle += data.angular.z*self.refresh_rate 
         self.angle = (self.angle+2*math.pi)%2*math.pi
-        
-        
+        self.vx= data.linear.x*np.cos(self.angle)
+        self.vy = data.linear.x*np.sin(self.angle)
+        self.x += self.vx*self.refresh_rate
+        self.y += self.vy*self.refresh_rate
+  
 def main(args=None):
     rclpy.init(args=args)
     m = Main()
