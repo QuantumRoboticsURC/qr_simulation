ac39380a0d2f8d46872fdf315adf93c2994f5ae5
A01749675 <A01749675@tec.mx>
2023-12-12 12:30:38 -0600

    Update with ROS

--- qr_simulation/main.py
+++ qr_simulation/main.py
@@ -5,9 +5,12 @@ from geometry_msgs.msg import Twist
 from .submodules.alvinxy import *
 import math
 import numpy as np
+from custom_interfaces.msg import TargetCoordinates,Coordinates
+
 class Main(Node):
     def __init__(self):
         self.subscribe = self.create_subscription(Twist,"cmd_vel",self.update_velocity)
+        self.coordinates = self.create_publisher(Coordinates,'/coordinates')
         self.linear_velocity = 0.0
         self.angular_velocity = 0.0
         self.angle = 0.0
@@ -23,10 +26,20 @@ class Main(Node):
     def update_data(self,data):
         self.angle += data.angular.z*self.refresh_rate 
         self.angle = (self.angle+2*math.pi)%2*math.pi
+        
         self.vx= data.linear.x*np.cos(self.angle)
         self.vy = data.linear.x*np.sin(self.angle)
+        
         self.x += self.vx*self.refresh_rate
         self.y += self.vy*self.refresh_rate
+        
+        coords = Coordinates()
+        lat,lon = xy2ll(self.x,self.y,self.orglat,self.orglong)
+        coords.latitude = lat
+        coords.longitude = lon
+        self.coordinates.publish(coords)
+        
+        
   
 def main(args=None):
     rclpy.init(args=args)
